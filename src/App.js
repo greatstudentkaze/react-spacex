6e91262d06746994ccb4a08907a0f7633a4dbb7e
Ivan Nefagin <dfyz0r@list.ru>
2020-10-14 11:50:55 +0300

    импортирует компоненты, стили

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
 
+import Header from './components/Header';
+import Main from './components/Main';
+
+import './css/style.css';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          :D
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Header />
+
+      <Main />
+    </React.Fragment>
   );
 }
 
