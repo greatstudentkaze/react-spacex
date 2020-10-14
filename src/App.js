50fca349687ce2966eec66ee18a9cff268bfbe02
Ivan Nefagin <dfyz0r@list.ru>
2020-10-14 13:34:30 +0300

    импортирует компоненты Features и Footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Header from './components/Header';
 import Main from './components/Main';
+import Features from './components/Features';
+import Footer from './components/Footer';
 
 import './css/style.css';
 
@@ -9,8 +11,9 @@ function App() {
   return (
     <React.Fragment>
       <Header />
-
       <Main />
+      <Features />
+      <Footer />
     </React.Fragment>
   );
 }
