e1ffa9d08b230cc99efc2133796d0245dc65d746
Ivan Nefagin <dfyz0r@list.ru>
2020-10-15 00:56:12 +0300

    добавляет рендер списка ракет и вешает на клик по элементам списка событие переключения ракет

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,23 +3,17 @@ import React from 'react';
 import './index.css';
 import logo from '../../img/logo.svg';
 
-const Header = () => (
+const Header = ({ rockets, changeRocket }) => (
   <header className="header">
     <img className="logo" src={logo} alt="Logo Space X"/>
     <nav className="main-nav nav">
       <ul className="list">
-        <li className="item">
-          <a href="/" className="item-link">Falcon 1</a>
-        </li>
-        <li className="item">
-          <a href="/" className="item-link">Falcon 9</a>
-        </li>
-        <li className="item">
-          <a href="/" className="item-link">Falcon Heavy</a>
-        </li>
-        <li className="item">
-          <a href="/" className="item-link">Updates</a>
-        </li>
+        {rockets.map((rocket, i) => (
+          <li className="item" key={i}>
+            <a href="/" className="item-link"
+               onClick={evt => {evt.preventDefault(); changeRocket(rocket)}}>{rocket}</a>
+          </li>
+        ))}
       </ul>
     </nav>
     <nav className="secondary-nav">
