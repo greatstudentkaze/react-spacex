e8f9673c373d9445effad82afb957be118f45db1
Ivan Nefagin <dfyz0r@list.ru>
2020-10-15 23:47:41 +0300

    реализует получение и рендер launches

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,76 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import Main from '../Main';
 
 import './index.css';
 
-const Calendar = () => (
-  <>
-    <Main />
-    <section className="calendar">
-      <div className="container">
-        <ul className="calendar-list">
-          <li className="calendar-item">
-            <article className="launches">
-              <div className="launches-image">
-                <img src="https://images2.imgbox.com/3c/0e/T8iJcSN3_o.png" alt="" />
-                <a className="launches-youtube" href="https://www.youtube.com/watch?v=dLQ2tZEH6G0" />
-              </div>
-              <div className="launches-content">
-                <h2 className="launches-title">FalconSat</h2>
-                <a href="./details.html" className="button launches-details">Подробнее</a>
-              </div>
-            </article>
-          </li>
-          <li className="calendar-item">
-            <article className="launches">
-              <div className="launches-image">
-                <img src="https://images2.imgbox.com/4f/e3/I0lkuJ2e_o.png" alt="" />
-              </div>
-              <div className="launches-content">
-                <h2 className="launches-title">
-                  <a href="https://www.space.com/3590-spacex-falcon-1-rocket-fails-reach-orbit.html">
-                    DemoSat
-                  </a>
-                </h2>
-                <a href="./details.html" className="button launches-details">Подробнее</a>
-              </div>
-            </article>
-          </li>
-          <li className="calendar-item">
-            <article className="launches">
-              <div className="launches-image">
-                <img src="https://images2.imgbox.com/3d/86/cnu0pan8_o.png" alt="" />
-              </div>
-              <div className="launches-content">
-                <h2 className="launches-title">
-                  <a href="http://www.spacex.com/news/2013/02/11/falcon-1-flight-3-mission-summary">
-                    Trailblazer
-                  </a>
-                </h2>
-                <a href="./details.html" className="button launches-details">Подробнее</a>
-              </div>
-            </article>
-          </li>
-          <li className="calendar-item">
-            <article className="launches">
-              <div className="launches-image">
-                <img src="https://images2.imgbox.com/e9/c9/T8CfiSYb_o.png" alt="" />
-              </div>
-              <div className="launches-content">
-                <h2 className="launches-title">
-                  <a href="https://en.wikipedia.org/wiki/Ratsat">
-                    RatSat
-                  </a>
-                </h2>
-                <a href="./details.html" className="button launches-details">Подробнее</a>
-              </div>
-            </article>
-          </li>
-        </ul>
-      </div>
-    </section>
-  </>
-);
+import FetchData from '../../service/FetchData';
+
+const fetchData = new FetchData();
+
+const Calendar = () => {
+
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetchData.getLaunches()
+      .then(launches => setData(launches));
+  }, []);
+
+  return (
+    <>
+      <Main/>
+      <section className="calendar">
+        <div className="container">
+          <ul className="calendar-list">
+            {data && data.map(launch => (
+              <li key={launch.id} className="calendar-item">
+                <article className="launches">
+                  <div className="launches-image">
+                    <img src={launch.links.patch.small} alt=""/>
+                  </div>
+                  <div className="launches-content">
+                    <h2 className="launches-title">{launch.name}</h2>
+                    <Link to="/details" className="button launches-details">More Details</Link>
+                  </div>
+                </article>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Calendar;
