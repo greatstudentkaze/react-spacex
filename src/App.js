3938b65cc7ce922c3973c7133975b84079edd0d7
Ivan Nefagin <dfyz0r@list.ru>
2020-10-15 22:40:59 +0300

    делает роутинг

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Home from './components/Home';
 import Main from './components/Main';
 import Features from './components/Features';
+import Calendar from './components/Calendar';
+import Details from './components/Details';
 import Footer from './components/Footer';
 
 import './css/style.css';
@@ -46,13 +49,30 @@ class App extends React.Component {
 
   render() {
     return (
-      <React.Fragment>
+      <BrowserRouter>
         <Header rockets={this.state.rockets} changeRocket={this.changeRocket} />
-        {this.state.company && <Home company={this.state.company} />}
-        {/*<Main rocket={this.state.rocket} />*/}
-        {/*{this.state.rocketFeatures && <Features {...this.state.rocketFeatures} />}*/}
+
+        <Route exact path='/'>
+          {this.state.company && <Home company={this.state.company} />}
+        </Route>
+
+        <Route path='/rocket'>
+          <Main rocket={this.state.rocket} />
+          {this.state.rocketFeatures && <Features {...this.state.rocketFeatures} />}
+        </Route>
+
+        <Route path="/calendar">
+          <Main />
+          <Calendar />
+        </Route>
+
+        <Route path="/details">
+          <Main />
+          <Details />
+        </Route>
+
         {this.state.company && <Footer {...this.state.company.links} />}
-      </React.Fragment>
+      </BrowserRouter>
     );
   };
 }
