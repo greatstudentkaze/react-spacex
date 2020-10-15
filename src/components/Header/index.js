555d1dee7a9f6baa0138db3964ac00b3bbe73d98
Ivan Nefagin <dfyz0r@list.ru>
2020-10-15 23:10:09 +0300

    импортирует Link, NavLink и использует их вместо a

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 import './index.css';
 import logo from '../../img/logo.svg';
 
 const Header = ({ rockets, changeRocket }) => (
   <header className="header">
-    <img className="logo" src={logo} alt="Logo Space X"/>
+    <Link to='/'>
+      <img className="logo" src={logo} alt="Logo Space X"/>
+    </Link>
     <nav className="main-nav nav">
       <ul className="list">
         {rockets.map((rocket, i) => (
           <li className="item" key={i}>
-            <a href="/" className="item-link"
-               onClick={evt => {evt.preventDefault(); changeRocket(rocket)}}>{rocket}</a>
+            <Link to="/rocket" className="item-link"
+               onClick={() => {changeRocket(rocket)}}>{rocket}</Link>
           </li>
         ))}
       </ul>
@@ -19,10 +22,10 @@ const Header = ({ rockets, changeRocket }) => (
     <nav className="secondary-nav">
       <ul className="list">
         <li className="item">
-          <a href="/" className="item-link">Home</a>
+          <NavLink exact to="/" className="item-link" activeClassName="active">Home</NavLink>
         </li>
         <li className="item">
-          <a href="calendar.html" className="item-link">Calendar</a>
+          <NavLink to="calendar" className="item-link" activeClassName="active">Calendar</NavLink>
         </li>
       </ul>
     </nav>
