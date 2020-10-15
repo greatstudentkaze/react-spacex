5b2173bebabf01ed30f1e624064db42418d038c2
Ivan Nefagin <dfyz0r@list.ru>
2020-10-15 23:45:50 +0300

    переносит Main из Features в Details

--- src/components/Features/index.js
+++ src/components/Features/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import RellaxWrapper from 'react-rellax-wrapper';
 
-import Main from '../Main';
-
 import './index.css';
 
 const rocketImages = {
@@ -13,51 +11,48 @@ const rocketImages = {
 }
 
 const Features = ({ description, diameter, height, mass, name, payload_weights: payloadWeights }) => (
-  <>
-    <Main/>
-    <section className="features">
-      <h2 className="features-title">
-        Falcon 1 <br/>Overview
-      </h2>
-      <div className="overview">
+  <section className="features">
+    <h2 className="features-title">
+      Falcon 1 <br/>Overview
+    </h2>
+    <div className="overview">
 
-        <table className="table">
-          <caption className="table-title">
-            Size
-          </caption>
-          <thead>
-          <tr>
-            <td className="table-column">HEIGHT</td>
-            <td className="table-column">{height.meters} m / {height.feet} ft</td>
-          </tr>
-          <tr>
-            <td className="table-column">DIAMETER</td>
-            <td className="table-column">{diameter.meters} m / {diameter.feet} ft</td>
-          </tr>
-          <tr>
-            <td className="table-column">MASS</td>
-            <td className="table-column">{mass.kg} kg / {mass.lb} lb</td>
-          </tr>
+      <table className="table">
+        <caption className="table-title">
+          Size
+        </caption>
+        <thead>
+        <tr>
+          <td className="table-column">HEIGHT</td>
+          <td className="table-column">{height.meters} m / {height.feet} ft</td>
+        </tr>
+        <tr>
+          <td className="table-column">DIAMETER</td>
+          <td className="table-column">{diameter.meters} m / {diameter.feet} ft</td>
+        </tr>
+        <tr>
+          <td className="table-column">MASS</td>
+          <td className="table-column">{mass.kg} kg / {mass.lb} lb</td>
+        </tr>
 
-          {payloadWeights.map(item => (
-              <tr key={item.id}>
-                <td className="table-column">PAYLOAD TO {item.id.toUpperCase()}</td>
-                <td className="table-column">{item.kg} kg / {item.lb} lb</td>
-              </tr>
-            )
-          )}
-          </thead>
-        </table>
-        <RellaxWrapper speed={14}>
-          <img className="rocket" src={`img/${rocketImages[name]}`} alt="rocket" />
-        </RellaxWrapper>
-        <article>
-          <h3 className="features-subtitle">DESCRIPTION</h3>
-          <p className="features-text">{description}</p>
-        </article>
-      </div>
-    </section>
-  </>
+        {payloadWeights.map(item => (
+            <tr key={item.id}>
+              <td className="table-column">PAYLOAD TO {item.id.toUpperCase()}</td>
+              <td className="table-column">{item.kg} kg / {item.lb} lb</td>
+            </tr>
+          )
+        )}
+        </thead>
+      </table>
+      <RellaxWrapper speed={14}>
+        <img className="rocket" src={`img/${rocketImages[name]}`} alt="rocket" />
+      </RellaxWrapper>
+      <article>
+        <h3 className="features-subtitle">DESCRIPTION</h3>
+        <p className="features-text">{description}</p>
+      </article>
+    </div>
+  </section>
 );
 
 export default Features;
