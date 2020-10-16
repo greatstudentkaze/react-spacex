cbef365b70d613f9de6df08a8f7ff0682cfb2e55
gsk <dfyz0r@list.ru>
2020-10-17 01:36:19 +0300

    добавляет функциональность кнопке Go back

--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -1,29 +1,34 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import './index.css';
 
 import Main from '../Main';
 
-const Details = () => (
-  <>
-    <Main />
-    <main className="details">
-      <div className="container">
-        <div className="details-row">
-          <div className="details-image">
-            <img src="https://images2.imgbox.com/3c/0e/T8iJcSN3_o.png" alt="" />
+const Details = () => {
+  const history = useHistory();
+
+  return (
+    <>
+      <Main />
+      <main className="details">
+        <div className="container">
+          <div className="details-row">
+            <div className="details-image">
+              <img src="https://images2.imgbox.com/3c/0e/T8iJcSN3_o.png" alt="" />
+            </div>
+            <div className="details-content">
+              <p className="details-description">Engine failure at 33 seconds and loss of vehicle</p>
+            </div>
           </div>
-          <div className="details-content">
-            <p className="details-description">Engine failure at 33 seconds and loss of vehicle</p>
+          <div>
+            <iframe className="details-youtube" width="560" height="315" src="https://www.youtube.com/embed/dLQ2tZEH6G0" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
           </div>
         </div>
-        <div>
-          <iframe className="details-youtube" width="560" height="315" src="https://www.youtube.com/embed/dLQ2tZEH6G0" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
-        </div>
-      </div>
-      <a href="calendar.html" className="button button-back">go back</a>
-    </main>
-  </>
-);
+        <button type="button" className="button button-back" onClick={history.goBack}>Go back</button>
+      </main>
+    </>
+  );
+};
 
 export default Details;
